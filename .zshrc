4ab9df09c3e41774135641cc4b513c88695e6541
Javier López Pardo <javier.lopezpardo@cabify.com>
2020-11-19 23:38:37 +0100

    fix some installation issues

---
+++ .zshrc
@@ -0,0 +1,3 @@
+export MACUP_BACKUP_PATH=~/Dropbox/dotfiles/macup
+export PATH=$PATH:~/.macup/bin
+export MACUP_BACKUP_PATH=~/Dropbox/dotfiles/macup
--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-set -u
-
 if [[ -d "$HOME/.macup" ]]; then
   echo "Mac up already installed. You can run macup update to update to latest version"
 else
@@ -11,7 +9,7 @@ else
 
   if [[ ":${PATH}:" != *"$HOME/.macup/bin:"* ]]; then
     echo "Adding macup binary to your PATH"
-    echo 'export PATH=$PATH:~/.macup/bin' >> .zshrc
+    echo 'export PATH=$PATH:~/.macup/bin' >> ~/.zshrc
   fi
 fi
 
@@ -22,7 +20,7 @@ BACKUP_PATH=${MACUP_BACKUP_PATH:-}
 if [[ -z $BACKUP_PATH ]]; then
   echo "Setting your backup path to ~/Dropbox/dotfiles/macup"
   echo 'You can modify this config by changing MACUP_BACKUP_PATH env variable'
-  echo 'export MACUP_BACKUP_PATH=~/Dropbox/dotfiles/macup' >> .zshrc
+  echo 'export MACUP_BACKUP_PATH=~/Dropbox/dotfiles/macup' >> ~/.zshrc
 fi
 
 echo "To start backing up, open a new terminal session and run"
