4cfb10126f6d66cb0dc20b66bf06b2e222cd68d8
Javier López Pardo <javier.lopezpardo@cabify.com>
2020-11-19 22:47:10 +0100

    fix install issues

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-set -up
-git clone git@github.com:jalopez/macup.git ~/.macup
+set -u
 
-if [[ ":${PATH}:" != *".macup/bin:"* ]]; then
-  echo "~/.macup/bin is not in your PATH. adding it"
+if [[ -d "$HOME/.macup" ]]; then
+  echo "Mac up already installed. You can run macup update to update to latest version"
+else
+  git clone git@github.com:jalopez/macup.git ~/.macup
+fi
+
+if [[ ":${PATH}:" != *"$HOME/.macup/bin:"* ]]; then
+  echo "Adding macup binary to your PATH"
   echo 'export PATH=$PATH:~/.macup/bin' >> .zshrc
 fi
 
-echo "Successfully installed macup. To start backing up, open a new session and run"
-echo "- macup for backing up"
-echo "- macup-restore for restoring a backup"
+echo "Successfully installed macup. "
+echo "To start backing up, open a new terminal session and run"
+echo "macup -h to see options"
