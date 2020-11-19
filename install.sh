7454ab3ce555a57b12540734ac9338f499b94dd9
Javier López Pardo <javier.lopezpardo@cabify.com>
2020-11-19 18:16:59 +0100

    more changes

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ git clone git@github.com:jalopez/macup.git ~/.macup
 
 if [[ ":${PATH}:" != *":~/.macup/bin:"* ]]; then
   echo "~/.macup/bin is not in your PATH. adding it"
-  echo "export PATH=\$PATH:~/.macup/bin >> .zshrc"
+  echo "export PATH=\$PATH:~/.macup/bin" >> .zshrc
 fi
 
 echo "Successfully installed macup. To start backing up, run macup in your console"
