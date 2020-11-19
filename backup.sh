def0f721f628890d81c8cd881933abda747ba1b5
Javier López Pardo <javier.lopezpardo@cabify.com>
2020-11-19 11:20:56 +0100

    use force option of dump

--- backup.sh
+++ backup.sh
@@ -7,7 +7,7 @@ BACKUP_TARGET=${MACUP_HOME:-"$HOME/Dropbox/dotfiles/macup"}
 echo "Saving files into $BACKUP_TARGET"
 
 echo "- Saving Brewconfig"
-(cd "$BACKUP_TARGET" && rm Brewfile && brew bundle dump)
+(cd "$BACKUP_TARGET" && brew bundle dump -f)
 
 echo "- Saving mac config"
 MACPREFS_BACKUP_DIR="$BACKUP_TARGET/macprefs" macprefs backup
