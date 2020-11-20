b6c7beabe8ec377d54026850bf2b2531bbe95ef3
Javier López Pardo <javier.lopezpardo@cabify.com>
2020-11-20 11:06:16 +0100

    improve installing

--- install.sh
+++ install.sh
@@ -5,25 +5,17 @@ if [[ -d "$HOME/.macup" ]]; then
 else
   git clone git@github.com:jalopez/macup.git ~/.macup
 
-  echo ""
   echo "Successfully installed macup."
 
   if [[ ":${PATH}:" != *"$HOME/.macup/bin:"* ]]; then
-    echo "Adding macup binary to your PATH"
-    echo 'export PATH=$PATH:~/.macup/bin' >> ~/.zshrc
-    export PATH=$PATH:~/.macup/bin
+    echo 'source ~/.macup/configure.sh' >> ~/.zshrc
+    source ~/.macup/configure.sh
   fi
 fi
 
-set -o nounset
-
-BACKUP_PATH=${MACUP_BACKUP_PATH:-}
-
-if [[ -z $BACKUP_PATH ]]; then
-  echo "Setting your backup path to ~/Dropbox/dotfiles/macup"
-  echo 'You can modify this config by changing MACUP_BACKUP_PATH env variable'
-  echo 'export MACUP_BACKUP_PATH=~/Dropbox/dotfiles/macup' >> ~/.zshrc
-  export MACUP_BACKUP_PATH=~/Dropbox/dotfiles/macup
-fi
+echo "Your backup path is set to ~/Dropbox/dotfiles/macup"
+echo 'You can modify this config by changing $MACUP_BACKUP_PATH env variable'
 
 macup setup
+
+echo "Macup properly configured! run 'macup -h' to see options"
