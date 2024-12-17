f594bfdb3529ed183d1c947fea28190fd9aa907f
Javier López <lopez.pardo@gmail.com>
2024-12-17 15:30:10 +0100

    bring back macprefs

--- lib/cmd/backup.sh
+++ lib/cmd/backup.sh
@@ -3,18 +3,18 @@
 function cmd_backup() {
     require_macup_configured
     print_header
-    
+
     lightblue "--- Running backup ---"
     echo -n "Saving files into "
     green "$MACUP_BACKUP_PATH"
-    
+
     lightblue "1. brew apps"
     (cd "$MACUP_BACKUP_PATH" && brew bundle dump -f)
-    
-    # lightblue "2. mac user preferences"
-    # MACPREFS_BACKUP_DIR="$MACUP_BACKUP_PATH/macprefs" macprefs backup
-    
-    # # Removing files handled by dotsync
-    # rm -rf "$MACUP_BACKUP_PATH/macprefs/dotfiles/*"
-    # rm -rf "$MACUP_BACKUP_PATH/macprefs/ssh/*"
+
+    lightblue "2. mac user preferences"
+    MACPREFS_BACKUP_DIR="$MACUP_BACKUP_PATH/macprefs" macprefs backup
+
+    # Removing files handled by dotsync
+    rm -rf "$MACUP_BACKUP_PATH/macprefs/dotfiles/*"
+    rm -rf "$MACUP_BACKUP_PATH/macprefs/ssh/*"
 }
--- lib/cmd/restore.sh
+++ lib/cmd/restore.sh
@@ -4,22 +4,22 @@ function cmd_restore {
     require_macup_configured
     require_backup_folder || exit 1
     print_header
-    
+
     lightblue "--- Restoring backup ---"
     echo -n "Reading files from "
     green "$MACUP_BACKUP_PATH"
-    
+
     lightblue "1. brew apps"
     (cd "$MACUP_BACKUP_PATH" && brew bundle -v)
-    
+
     # lightblue "2. mac user preferences"
-    # MACPREFS_BACKUP_DIR="$MACUP_BACKUP_PATH/macprefs" macprefs restore
-    
+    MACPREFS_BACKUP_DIR="$MACUP_BACKUP_PATH/macprefs" macprefs restore
+
     lightblue "3. dotfiles"
     (cd ~ && dotsync -f)
-    
+
     lightblue "4. custom postinstall script"
-    [[ -f  $MACUP_BACKUP_PATH/postinstall.sh ]] && zsh "$MACUP_BACKUP_PATH/postinstall.sh"
-    
+    [[ -f $MACUP_BACKUP_PATH/postinstall.sh ]] && zsh "$MACUP_BACKUP_PATH/postinstall.sh"
+
     green "Restore successfully done. Enjoy!"
 }
